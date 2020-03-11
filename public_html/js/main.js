12e58141f1b4d22f061f19e97559d40bdca4c291
Marcelo Júnior <rokermarcelo@gmail.com>
2020-03-11 11:30:24 -0300

    Campo de digitação e contagem dos caracteres e palavras da resposta

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,13 +1,23 @@
 
+// * Obtém o número de palavras da frase *************************************//
 var frase = $(".frase");
-
-console.log(frase);
-console.log(frase.text());
-
-var palavras = frase.text().split(" ");
-
-console.log(palavras);
-console.log(palavras.length);
-
+//console.log(frase);
+//console.log(frase.text());
+var palavrasFrase = frase.text().split(" ");
+//console.log(palavras);
+//console.log(palavras.length);
 var spanTamanho = $("#tamanho");
-spanTamanho.text(palavras.length);
+spanTamanho.text(palavrasFrase.length);
+
+// * Atualiza contadores da resposta *****************************************//
+var contadorDeCaracteres = $("#contadorDeCaracteres");
+var contadorDePalavras = $("#contadorDePalavras");
+
+var campoDeDigitacao = $(".campoDeDigitacao");
+campoDeDigitacao.on("input", function(){
+    var conteudo = campoDeDigitacao.val();
+    var palavras = conteudo.split(/\S+/);
+    contadorDeCaracteres.text(conteudo.length);
+    contadorDePalavras.text(palavras.length - 1);
+});
+
