cc128a39ddb4ab01cfba1f9f5efba08d81c17cca
Marcelo Júnior <rokermarcelo@gmail.com>
2020-03-21 12:01:06 -0300

    Função de tempo decrescente, botão de reiniciar e refatoração do código

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,23 +1,55 @@
-
-// * Obtém o número de palavras da frase *************************************//
+// * Obtém referência para os elementos da página ****************************//
 var frase = $(".frase");
-//console.log(frase);
-//console.log(frase.text());
-var palavrasFrase = frase.text().split(" ");
-//console.log(palavras);
-//console.log(palavras.length);
 var spanTamanho = $("#tamanho");
-spanTamanho.text(palavrasFrase.length);
-
-// * Atualiza contadores da resposta *****************************************//
+var spanTempo = $("#tempo");
 var contadorDeCaracteres = $("#contadorDeCaracteres");
 var contadorDePalavras = $("#contadorDePalavras");
-
 var campoDeDigitacao = $(".campoDeDigitacao");
-campoDeDigitacao.on("input", function(){
+var btnReiniciar = $("#btnReiniciar");
+
+// * Variáveis auxiliares à lógica da aplicação ******************************//
+var palavrasFrase = frase.text().split(" ");
+var tempoTotal = spanTempo.text();
+var tempo = 0;
+var cronometro = null;
+
+// * Configura valores e comportamentos dos elementos da página **************//
+spanTamanho.text(palavrasFrase.length);
+
+campoDeDigitacao.on("input", digitar);
+
+btnReiniciar.on("click", reiniciar);
+
+// * Funções da aplicação ****************************************************//
+
+function iniciarCronometro(){
+    tempo = spanTempo.text();
+    cronometro = setInterval(function(){
+        tempo--;
+        spanTempo.text(tempo);
+        if(tempo < 1){
+            campoDeDigitacao.attr("disabled", true);
+            clearInterval(cronometro);
+            cronometro = null;
+        }
+    }, 1000);
+}
+
+function digitar(){
+    if(cronometro === null){
+        iniciarCronometro();
+    }
     var conteudo = campoDeDigitacao.val();
     var palavras = conteudo.split(/\S+/);
     contadorDeCaracteres.text(conteudo.length);
     contadorDePalavras.text(palavras.length - 1);
-});
+}
 
+function reiniciar() {
+    campoDeDigitacao.attr("disabled", false);
+    campoDeDigitacao.val("");
+    campoDeDigitacao.focus();
+    contadorDeCaracteres.text(0);
+    contadorDePalavras.text(0);
+    spanTempo.text(tempoTotal);
+}
