b0c7fbc48345677b99dfe579bb13975323cc36ed
Marcelo Júnior <rokermarcelo@gmail.com>
2020-03-23 18:14:48 -0300

    Adicionado botão que permite ao usuário buscar por uma frase específica (via ID)

--- public_html/js/frase.js
+++ public_html/js/frase.js
@@ -1,10 +1,49 @@
 $("#btnTrocarFrase").click(fraseAleatoria);
+$("#btnFraseId").click(frasePorId);
+
+function frasePorId(){
+    var fraseId = $("#fraseId").val();
+    var dados = { id: fraseId};
+    $("#spinner").show();
+    $.get(
+        "http://localhost:3000/frases",
+        dados,
+        function(data){
+            frase.text(data.texto);
+            setTamanhoFrase(frase.text());
+            setTempo(data.tempo);
+        }
+    )
+    .fail(function(){
+        $("p.erro").show();
+        $("p.erro").fadeOut(2000);
+
+        setTimeout(function(){
+            $("p.erro").hide();
+        }, 2000);
+    })
+    .always(function(){
+        $("#spinner").hide();
+    });
+}
 
 function fraseAleatoria(){
+    $("#spinner").show();
     $.get(
         "http://localhost:3000/frases",
         trocaFrase
-    );
+    )
+    .fail(function(){
+        $("p.erro").show();
+        $("p.erro").fadeOut(2000);
+
+        setTimeout(function(){
+            $("p.erro").hide();
+        }, 2000);
+    })
+    .always(function(){
+        $("#spinner").hide();
+    });
 }
 
 function trocaFrase(data) {
